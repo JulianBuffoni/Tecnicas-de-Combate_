b769511d9a0b419ce3ce7ce91e2c27992dfb3384
JulianBuffoni <jbuffoni@frba.utn.edu.ar>
2021-04-23 16:56:03 -0300

    Finished

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,49 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+type Golpe = Number
+
+--1
+
+poderGolpe :: Horas -> Number
+poderGolpe = (15*)
+
+fortalezaObjetivo :: Objetivo -> Number
+fortalezaObjetivo = (2*) . length
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poderGolpe horas / fortalezaObjetivo objetivo
+
+--2
+
+-- Gomu Gomu Elephant Gatling = gomuGomu
+-- Golpes Normales Consecutivos = golpesNormales
+
+gomuGomu :: Golpe
+gomuGomu = poderGolpe 180
+
+golpesNormales :: Golpe
+golpesNormales = poderGolpe 240
+
+presionGomuGomu :: Objetivo -> Presion
+presionGomuGomu objetivo = gomuGomu / fortalezaObjetivo objetivo
+
+--3
+
+esDificil :: Objetivo -> Bool
+esDificil objetivo =  presionGomuGomu objetivo < 100
+
+focalizar :: Objetivo -> String
+focalizar = take 7
+
+presionNormalesConsecutivos :: Objetivo -> Presion
+presionNormalesConsecutivos objetivo = golpesNormales / fortalezaObjetivo objetivo
+
+focaliza :: Presion -> Bool
+focaliza presion = presion >= 200 && presion <= 400
+
+esAccesible :: Objetivo -> Bool
+esAccesible = focaliza . presionNormalesConsecutivos . focalizar
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,16 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
-
+  describe "presionGolpe" $ do
+    it "Funcion No hace Kabool" $ do
+      presionGolpe 200 "bolsa de entrenamiento" `shouldBe` 68.181818182
+  describe "esDificil" $ do
+      it "Ta complicao" $ do
+        esDificil "bolsa de entrenamiento" `shouldBe` True
+      it "EZ" $ do
+        esDificil "puf" `shouldBe` False
+  describe "esAccesible" $ do
+      it "Puedo :)" $ do
+        esAccesible "bolsa de entrenamiento" `shouldBe` True
+      it "Let me in!!! Let me in!!!" $ do
+        esAccesible "puf" `shouldBe` False
